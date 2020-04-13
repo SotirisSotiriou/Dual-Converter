232ef9b61baa3c8a2e761baeaeaf8d4026a03437
Sotiris Sotiriou <45819898+sotiris2000@users.noreply.github.com>
2020-04-13 14:20:22 +0300

    update Dual_Cnverter.py ( method writeNewFile(file,c,A,b,eqin,maxmin) )

--- Dual Converter/Dual_Converter.py
+++ Dual Converter/Dual_Converter.py
@@ -1,17 +1,23 @@
 from Parser import parse
 
 def convert(filename):
-    c = []
-    A = []
-    b = []
-    Eqin = []
-    maxmin = []
-    c, A, b, maxmin, Eqin = parse(filename)
+    cDual = []
+    ADual = []
+    bDual = []
+    eqinDual = []
+    maxminDual = []
+    #input
+    cPrimal, APrimal, bPrimal, maxminPrimal, eqinPrimal = parse(filename)
+
+    #convertion
 
 
 
-def export(oldfilename):
-    createNewFile(oldfilename)
+
+
+def export(oldfilename, c, A, b, eqin, maxmin):
+    newfile = createNewFile(oldfilename)
+    writeNewFile(newfile, c, A, b, eqin, maxmin)
 
 
 
@@ -42,9 +48,44 @@ def createNewFile(filename):
         return file
 
 
+
+def writeNewFile(file, c, A, b, eqin, maxmin, variables): 
+    #the tables c, A, b, Equin, maxmin, variables are refering to dual problem
+    if maxmin == 1:
+        file.write("max ")
+    elif maxmin == -1:
+        file.write("min ")
+    file.write("z = ")
+    
+    index = 0
+    while index < len(c):
+        file.write(str(c[index]))
+        file.write(str(variables[index]))
+    file.write("\ns.t.\n")
+    for row in range(0,len(A)):
+        for col in range(0, len(A[0])):
+            if A[row][col] >= 0:
+                file.write('+' + str(A[row][col]))
+                file.write(str(variables[col]) + " ")
+            else:
+                file.write(str(A[row][col]))
+                file.write(str(variables[col]) + " ")
+        if eqin[row] == -1:
+            file.write("<= ")
+            file.write(str(b[row]))
+        elif eqin[row] == 0:
+            file.write("= ")
+            file.write(str(b[row]))
+        elif eqin[row] == 1:
+            file.write(">= ")
+            file.write(str(b[row]))
+
+
+
 def main():
     filename = str(input("\nEnter problem file name: "))
     convert(filename)
+
 
 if __name__ == "__main__":
     main()
